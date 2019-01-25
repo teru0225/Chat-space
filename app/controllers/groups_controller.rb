c697c9e580cf78e7f1be9d35069f15bfe28e1586
teru0225 <teruuuuu20@gmail.com>
2019-01-25 18:17:54 +0900

    Edited group.controller.rb
    グループを作成するとき、viewでは、user_idsというキーのバリューに、 グループに追加するuserのidを設定して
    paramsとしてcontrollerに送信するため。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, { :user_ids => [] })
 end
 end
