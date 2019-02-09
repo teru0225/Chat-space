fbd145b20f9828109610478341705470a4c66900
teru0225 <teruuuuu20@gmail.com>
2019-02-09 16:10:53 +0900

    Edited spec/models/message_spec.rb
    ４点のエラーが出ていたため、その習性を行った

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: model do
+RSpec.describe Message, type: :model do
   describe '#create' do
     context 'can save' do
       it 'is valid with body' do
@@ -20,19 +20,19 @@ RSpec.describe Message, type: model do
       it 'is invalid without body and image' do
         message = build(:message, body: nil, image: nil)
         message.valid?
-        expect(message.errors[:body]).to include('入力をしてください')
+        expect(message.errors[:body]).to include('を入力してください')
       end
 
       it 'is invalid without group_id' do
         message = build(:message, group_id: nil)
         message.valid?
-        expect(message.errors[:group]).to include('入力をしてください')
+        expect(message.errors[:group]).to include('を入力してください')
       end
 
       it 'is invalid without user_id' do
         message = build(:message, user_id: nil)
         message.valid?
-        expect(message.errors[:user]).to include('入力をしてください')
+        expect(message.errors[:user]).to include('を入力してください')
       end
     end
   end
