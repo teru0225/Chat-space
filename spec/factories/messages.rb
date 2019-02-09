478bd69aa5f136ca6b435bfb20f18013537d5c9b
teru0225 <teruuuuu20@gmail.com>
2019-02-09 16:10:15 +0900

    Edited factories/messages.rb
    カラム名を間違って、コードしていたため、その修正を行った。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :message do
-    content Faker::Lorem.sentence
+    body Faker::Lorem.sentence
     image File.open("#{Rails.root}/public/images/no_image.jpg")
     user
     group
