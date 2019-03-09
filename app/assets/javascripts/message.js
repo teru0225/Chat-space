bb119f36313ce0042f4aff61d75c7aa99091b805
teru0225 <teruuuuu20@gmail.com>
2019-03-09 13:39:16 +0900

    made new file message.js
    メッセージが送信されるとイベントが起こるようにした。
    Submitをクリックすることで、イベントが発生するようになっている。
    このイベントが発生すると、フォームに入力された値がコントローラに送られるようになっている。
    コントローラ内で、HTMLとJSONの場合で処理をわけ、jbuilderを使って返すデータを作成するようにしている。
    Doneメソッドを使うことで、受け取っらデータをビューに反映するようにした。
    非同期に失敗した場合には、「メッセージの送信に失敗しました」というエラー文が出るようにした。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function() {
+  function buildHTML(message){
+    var image = message.image == null ? "" : `<img src="${message.image}" class="main__Body--image">`
+    var html = `<div class="main__Body--main clearfix">
+                  <div class="main__Body--name">
+                    ${message.user_name}
+                  </div>
+                  <div class="main__Body--date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="main__Body--text">
+                    <p class="main__Body--content">
+                      ${message.body}
+                    </p>
+                    ${image}
+                </div> `
+        return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = window.location.href
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.main__Body').append(html);
+      $('.main__Footer--submit').prop( "disabled", false );
+
+      $('.main__Body').animate({scrollTop: $('.main__Body')[0].scrollHeight}, 'fast');
+      $('.main__Footer--placeholder').val('');
+      $('hidden').val('');
+      })
+    .fail(function(){
+      alert('メッセージの送信に失敗しました');
+    })
+  })
+});
+
